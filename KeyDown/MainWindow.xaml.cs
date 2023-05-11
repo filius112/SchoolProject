265d4491296f262bd3caa0dee099ace4e1677121
filius112 <hanoutfilip@gmail.com>
2023-05-11 23:05:01 +0200

    v1.1.7

--- KeyDown/MainWindow.xaml.cs
+++ KeyDown/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Data.SQLite;
+using System.IO;
+using System.Configuration;
+using System.Xml.Linq;
+using KeyDown.Models;
 
 namespace KeyDown
 {
@@ -23,30 +29,182 @@ namespace KeyDown
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
+        // Specify the name of the new database file
+        const string saveScore = "MyDatabase.db";
+
+        // Create a new SQLite database file
+
+        const string ConnectStr = $"Data Source={saveScore}; Version=3;";
+
+        private void CreateDB()
+        {
+            if (!File.Exists(saveScore))
+            {
+                SQLiteConnection.CreateFile(saveScore);
+                using (SQLiteConnection conn = new SQLiteConnection(ConnectStr))
+                {
+                    string savedScore = "CREATE TABLE IF NOT EXISTS Saves(" + "ID INTEGER PRIMARY KEY AUTOINCREMENT," + "score INTEGER NOT NULL);";
+                    conn.Open();
+                    SQLiteCommand cmd2 = new SQLiteCommand(savedScore, conn);
+                    cmd2.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+
+        void SeedData()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectStr))
+            {
+                conn.Open();
+                string cmd = "INSERT INTO Saves(score)VALUES" +
+                    "(20)," +
+                    "(29)," +
+                    "(31);";
+                SQLiteCommand CMD = new SQLiteCommand(cmd, conn);
+                CMD.ExecuteNonQuery();
+                conn.Close();
+                UpdateHiscore();
+            }
+        }
+
+        private List<saves> ObtainData()
+        {
+            List<saves> data = new List<saves>();
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectStr))
+            {
+                string cmd = "SELECT * FROM Saves";
+                conn.Open();
+                using (SQLiteCommand sqlcmd = new SQLiteCommand(cmd, conn))
+                {
+                    SQLiteDataReader reader = sqlcmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        data.Add(new saves
+                        {
+                            score = int.Parse(reader["score"].ToString())
+                        });
+                    }
+                }
+                conn.Close();
+            }
+            return data;
+        }
+        void DeleteDB()
+        {
+            if (File.Exists(saveScore))
+            {
+                File.Delete(saveScore);
+            }
+        }
+
+        List<saves> Saves { get; set; }
+
+
+
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        // Set the initial count to 0
+        int count = 0;
+
+        double score = 0;
+
+
         public MainWindow()
         {
-         DispatcherTimer hod = new DispatcherTimer();
-            hod.Interval = new TimeSpan(0,0, 6/3);
-
-
+            CreateDB();
+          //DeleteDB();
+            SeedData();
+            Saves = ObtainData();
             InitializeComponent();
+            UpdateHiscore();
+
+            // Set the interval between timer ticks (in milliseconds)
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
+
+            // Set the event handler for the Tick event
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+
+            Jitter.IsEnabled = false;
+
         }
 
-        int score = 0;
-
-        private void main_Click(object sender, RoutedEventArgs e)
+         void DispatcherTimer_Tick(object sender, EventArgs e)
         {
-            score += 1;
+            // Increment the count
+            count++;
 
-            Count.Text = score.ToString();
+            // If the count is greater than or equal to 10, stop the timer
+            if (count >= 10)
+            {
+                dispatcherTimer.Stop();
+                startButton.IsEnabled = true; // Re-enable the button
+                count = 0;
+                Jitter.IsEnabled = false;
+                hiscore.Text = (score/10 + " clicks/s").ToString();
+                cps.Text = (score / 10 + " clicks/s").ToString();
+                UpdateHiscore();
+                
+            }
+
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectStr))
+            {
+                conn.Open();
+
+                string cmd = "INSERT INTO Saves(score) VALUES (@score);";
+
+                using (SQLiteCommand sqlcmd = new SQLiteCommand(cmd, conn))
+                {
+                    sqlcmd.Parameters.AddWithValue("@score", score);
+                    sqlcmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
+
+
+            // Update the TextBlock with the current count
+            laabel.Content = count.ToString();
+
         }
 
-        private async void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            int i = 0;
-            await Task.Delay(1000);
+            
+            // Disable the button while the timer is running
+            startButton.IsEnabled = false;
+            Jitter.IsEnabled = true;
 
-            Count.Text = score.ToString();
+            // Start the timer after 1 seconds
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Start();
+            score = 0;
+            Counter.Text = score.ToString();
         }
+
+        private void Jitter_Click(object sender, RoutedEventArgs e)
+        {
+            score++;
+
+            Counter.Text = score.ToString();
+        }
+        private void UpdateHiscore()
+        {
+            if (Saves != null && Saves.Count > 0)
+            {
+                // Obtain the saved data from the database
+                Saves = ObtainData();
+
+                // Calculate the highest score
+                double highestScore = Saves.Max(s => s.score);
+
+                // Set the Text property of the hiscore label
+                hiscore.Text = $"{highestScore/10} clicks/s";
+            }
+        }
+
+
     }
 }
---
+++ KeyDown/Models/saves.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyDown.Models
+{
+    public class saves
+    {
+        public int score { get; set; }
+
+         public override string ToString()
+         {
+             return $"({score})";
+         }
+    }
+}
